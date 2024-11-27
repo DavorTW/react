730d1ad42893e26147584b301968b683ab22b973
DavorLnx <138895434+DavorTW@users.noreply.github.com>
2024-11-27 22:20:26 +0800

    added test folder

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { App, } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
-import Student from './Student';
+//import { FirstApp } from './FirstApp';
+//import Student from './Student';
+import CounterApp from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Student name='Davor' age={26} isStudent={true}/>
-        <Student name='John' age={32} isStudent={false}/>
-        <Student/>
+        <CounterApp value={0}/>
     </React.StrictMode>
-)
+);
