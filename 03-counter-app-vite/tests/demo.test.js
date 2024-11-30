368e72fadff3c94eb8db6817958909867612d44c
DavorLnx <138895434+DavorTW@users.noreply.github.com>
2024-11-30 15:52:44 +0800

    Finished initial configurations for jest and babel Installed types for easier workflow with tests Finished first demo test

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,5 +1,14 @@
-test('Esta prueba no debe de fallar', () => {
-    if (1 === 0) {
-        throw new Error('No se puede dividir entre cero')
-    }
-})
+describe('Pruebas en <DemoComponent></DemoComponent>', () => {
+    test('Esta prueba no debe de fallar', () => {
+        //1. inicializacion (Arrange)
+        const message1 = 'Hola Mundo';
+    
+        //2. estimulo (Act)
+        const message2 = message1.trim();
+    
+        //3. observar el comportamiento (Assert)
+        expect( message1 ).toBe( message2 );
+    });
+});
+
+
